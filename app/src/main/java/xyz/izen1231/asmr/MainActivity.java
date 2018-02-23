ede96107f930511b170f3878d1145a811e21336a
izen1231 <izen1231@gmail.com>
2018-02-23 17:54:13 +0900

    메인 xml

--- app/src/main/java/xyz/izen1231/asmr/MainActivity.java
+++ app/src/main/java/xyz/izen1231/asmr/MainActivity.java
@@ -1,5 +1,6 @@
 package xyz.izen1231.asmr;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
     }
 
