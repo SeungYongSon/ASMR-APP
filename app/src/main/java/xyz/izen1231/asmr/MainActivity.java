83dc86d64de504b15e7b38f21adb8863ac912797
izen1231 <izen1231@gmail.com>
2018-02-23 17:10:34 +0900

    toast message add

--- app/src/main/java/xyz/izen1231/asmr/MainActivity.java
+++ app/src/main/java/xyz/izen1231/asmr/MainActivity.java
@@ -2,6 +2,7 @@ package xyz.izen1231.asmr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-}
+
+        @Override
+        public void onBackPressed() {
+            Toast.makeText(this, "뒤로가기 버튼을 한번 더 눌러주세요", Toast.LENGTH_SHORT).show();
+            super.onBackPressed();
+        }
+
+    }
+
